3c2dafab0bc23d12b360ede91b87a39ade2471ca
BuildTools <unconfigured@null.spigotmc.org>
2025-08-19 15:03:42 -0400

    Added ability to go to wiki not of your display language

--- SCP Finder/popup.js
+++ SCP Finder/popup.js
@@ -2,6 +2,8 @@ const uiLang = chrome.i18n.getUILanguage();
 
 const scpInput = document.getElementById("scpNumber");
 const findButton = document.getElementById('findButton');
+const findOtherLangButton1 = document.getElementById('findOtherLangButton1');
+const findOtherLangButton2 = document.getElementById('findOtherLangButton2');
 console.log("Detected UI language:", uiLang);
 
 var scpNumber = scpInput.value;
@@ -10,6 +12,8 @@ var wiki_branch;
 document.getElementById('title').textContent = chrome.i18n.getMessage('ext_name');
 scpInput.textContent = chrome.i18n.getMessage('scp_input_placeholder');
 findButton.textContent = chrome.i18n.getMessage('find_button');
+findOtherLangButton1.textContent = chrome.i18n.getMessage('find_other_lang_1_button');
+findOtherLangButton2.textContent = chrome.i18n.getMessage('find_other_lang_2_button');
 
 scpInput.focus();
 
@@ -18,7 +22,7 @@ if (uiLang.startsWith('ja')) {
 } else if (uiLang.startsWith('es')) {
 	wiki_branch = 'https://lafundacionscp.wikidot.com/scp-';
 } else {
-	wiki_branch = "http://scp-wiki.wikidot.com/scp-"
+	wiki_branch = "https://scp-wiki.wikidot.com/scp-"
 }
 
 function fixInputNumber(input) {
@@ -33,16 +37,35 @@ scpInput.addEventListener("keyup", ({key}) => {
     }
 })
 
-function goToSCP() {
+function goToSCP(_wiki) {
 	//Updates scpNumber var
 	scpNumber = scpInput.value;
 	
 	console.log("SCP " + fixInputNumber(scpNumber));
-	window.open(wiki_branch+fixInputNumber(scpNumber));
+	window.open(_wiki+fixInputNumber(scpNumber));
 }
 
 findButton.addEventListener("click", function() {
-	goToSCP();
+	goToSCP(wiki_branch);
+});
+
+findOtherLangButton1.addEventListener("click", function() {
+	let otherWiki = "";
+	if (uiLang.startsWith('en')) {
+		otherWiki = "https://lafundacionscp.wikidot.com/scp-"
+	} else {
+		otherWiki = "https://scp-wiki.wikidot.com/scp-";
+	}
+	goToSCP(otherWiki);
+});
+findOtherLangButton2.addEventListener("click", function() {
+	let otherWiki = "";
+	if (uiLang.startsWith('ja')) {
+		otherWiki = "https://lafundacionscp.wikidot.com/scp-"
+	} else {
+		otherWiki = "http://scp-jp.wikidot.com/scp-";
+	}
+	goToSCP(otherWiki);
 });
 
 //stops non-number input
